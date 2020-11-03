dad995c7e1b1fcff2de47840f37dfd0acf4981c6
ZibinRoman <61138127+ZibinRoman@users.noreply.github.com>
2020-11-03 15:40:17 +0500

    Create info.php

---
+++ Cars/lab5/info.php
@@ -0,0 +1,10 @@
+<?require_once 'engine/page/title.php';?>
+<?require_once 'engine/connection/connectionStart.php';?>
+<html>
+    <body>
+		<?
+			
+		?>
+	</body>
+</html>
+<?require_once 'engine/connection/connectionEnd.php';?>
