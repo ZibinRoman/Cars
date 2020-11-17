9db46ae4a7eddef67f39df0b29cdba935df2a478
ZibinRoman <61138127+ZibinRoman@users.noreply.github.com>
2020-11-17 13:10:37 +0500

    Edit

--- Cars/lab5/index.php
+++ Cars/lab5/index.php
@@ -5,20 +5,55 @@
     <body>
 		<?
             echo("<div>");
-            echo("<div><h1><i> <a href='/lab5/info.php?Index="."cars"."'> Показать таблицу машину</a> </i></h1> </div>");
-            echo("<div><h1><i> <a href='/lab5/info.php?Index="."seller"."'> Показать таблицу автосалонов</a> </i></h1> </div>");
-            echo("<div><h1><i> <a href='/lab5/info.php?Index="."stock"."'> Показать таблицу автомобили в наличии</a> </i></h1> </div>");
+            echo("<div><p><i> <a href='/lab5/info.php?Index="."cars"."'> Показать таблицу машину</a> </i></p> </div>");
+            echo("<div><p><i> <a href='/lab5/info.php?Index="."seller"."'> Показать таблицу автосалонов</a> </i></p> </div>");
+            echo("<div><p><i> <a href='/lab5/info.php?Index="."stock"."'> Показать таблицу автомобили в наличии</a> </i></p> </div>");
+            echo("</div>");
+            $queryTab = "cars";
+            $headText = "Таблица машин";
+            $arrayTitle = array("№","Марка", "Модель", "Год выпуска", "Трансмиссия", "Объем выпуска", "Стоимость", "Изменить", "Добавить");
+            $query = "SELECT * FROM $database.$queryTab  ORDER BY $database.$queryTab.id ASC";
+            $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
+            echo("<div>");
+            $a = new Table($headText, $arrayTitle, $result, $queryTab, true);
+            echo("</div>");
+            
+            $queryTab = "seller";
+            $headText = "Таблица автосалонов";
+            $arrayTitle = array("№","Название", "Адресс", "Изменить", "Добавить");
+            $query = "SELECT * FROM $database.$queryTab  ORDER BY $database.$queryTab.id ASC";
+            $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
+            echo("<div>");
+            $a = new Table($headText, $arrayTitle, $result, $queryTab, true);
+            echo("</div>");
+            
+            $queryTab = "stock_info";
+            $headText = "Автомобили в наличии";
+            $arrayTitle = array("№","Автомобиль", "Автосалон", "Цена",  "Изменить", "Добавить");
+            $query = "SELECT * FROM $database.$queryTab  ORDER BY $database.$queryTab.id ASC";
+            $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
+            echo("<div>");
+            $a = new Table($headText, $arrayTitle, $result, $queryTab, true);
+            echo("</div>");
+            
+            $queryTab = "model_count_info";
+            $headText = "Количество автомобилей в автосалонах";
+            $arrayTitle = array("Автомобиль", "Количество");
+            $query = "SELECT * FROM $database.$queryTab";
+            $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
+            echo("<div>");
+            $a = new Table($headText, $arrayTitle, $result, $queryTab, false);
             echo("</div>");
             
             echo("<div>");
-            echo("<div><h1><i> <a href='/lab5/gen_pdf.php'> Показать pdf </a> </i></h1> </div>");
-            echo("<div><h1><i> <a href='/lab5/gen_xls.php'> Показать xls </a> </i></h1> </div>");
+            echo("<div><p><i> <a href='/lab5/gen_pdf.php'> Показать pdf </a> </i></p> </div>");
+            echo("<div><p><i> <a href='/lab5/gen_xls.php'> Показать xls </a> </i></p> </div>");
             echo("</div>");
 
             echo("<div>");
-            echo("<div><h1><i> <a href='/lab5/new.php?Index="."cars"."'> Добавить новую машину</a> </i></h1> </div>");
-            echo("<div><h1><i> <a href='/lab5/new.php?Index="."seller"."'> Добавить новый автосалон</a> </i></h1> </div>");
-            echo("<div><h1><i> <a href='/lab5/new.php?Index="."stock"."'> Добавить новый автомобиль в салон</a> </i></h1> </div>");
+            echo("<div><p><i> <a href='/lab5/new.php?Index="."cars"."'> Добавить новую машину</a> </i></p> </div>");
+            echo("<div><p><i> <a href='/lab5/new.php?Index="."seller"."'> Добавить новый автосалон</a> </i></p> </div>");
+            echo("<div><p><i> <a href='/lab5/new.php?Index="."stock"."'> Добавить новый автомобиль в салон</a> </i></p> </div>");
             echo("</div>");
             echo("<div>");
             echo("<div> <a href='https://yadi.sk/i/KCkhwrY0V9ipzQ'> Загрузить отчет </a> </div>");
